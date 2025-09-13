27907cb94707b3d4e06430ca40ebfed7af332915
dangnam1834 <nam.dang@mirrorfuturediamond.com>
2025-09-13 19:42:07 +0700

    update section 1 v0.1

--- src/components/home/section1/Section1.jsx
+++ src/components/home/section1/Section1.jsx
@@ -1,14 +1,15 @@
-import './Section1.css';
+import "./Section1.css";
 
 const Section1 = () => {
   return (
     <section className="section1">
-      <h1>Section 1</h1>
-      <p>Content for section 1</p>
-
       {/* Logo positioned at bottom */}
       <div className="section1-logo-container">
-        <img src="/icon/Icon/Số Ít logo.svg" alt="Số Ít" className="section1-logo" />
+        <img
+          src="/icon/Icon/Số Ít logo.svg"
+          alt="Số Ít"
+          className="section1-logo"
+        />
       </div>
     </section>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,7 +13,7 @@ import Section11 from "@components/home/section11/Section11";
 const HomePage = () => {
   return (
     <div className="home-page">
-      {/* <Section1 /> */}
+      <Section1 />
       <Section2 />
       <Section3 />
       <Section4 />
