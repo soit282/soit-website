3aee436a56e271dda3f0ae8eab5349d84d09d2a2
dangnam1834 <nam.dang@mirrorfuturediamond.com>
2025-09-14 17:01:20 +0700

    add section 1 2 3 4 for about page

---
+++ src/components/about/section2-about/Section2About.jsx
@@ -0,0 +1,26 @@
+import "./Section2About.css";
+
+const Section2About = () => {
+  return (
+    <section className="section2-about">
+      <div className="container">
+        <div className="content-grid">
+          <div className="left-column">
+            <p className="text-5">
+              We approach every project with curiosity and a willingness to
+              challenge norms. Exploration is how we unlock fresh ideas, cross
+              boundaries between disciplines, and stay ahead of the curve. It's
+              not just about seeking inspiration — it's about creating the
+              unexpected with intention, depth, and a spirit of discovery.
+            </p>
+          </div>
+          <div className="right-column">
+            <h2 className="text-2">Explorative</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Section2About;
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,16 @@
 import ServiceOffer from "@components/services/ServiceOffer";
+import Section1About from "@components/about/section1-about/Section1About";
+import Section2About from "@components/about/section2-about/Section2About";
+import Section3About from "@components/about/section3-about/Section3About";
+import Section4About from "@components/about/section4-about/Section4About";
 
 const AboutPage = () => {
   return (
     <div className="about-page">
+      <Section1About />
+      <Section2About />
+      <Section3About />
+      <Section4About />
       <ServiceOffer />
     </div>
   );
