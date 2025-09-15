485f074ab40ddcc216ed89adae56a7836d419bd5
dangnam1834 <nam.dang@mirrorfuturediamond.com>
2025-09-15 21:08:05 +0700

    fixed reponsive v0.2

--- src/components/common/ArrowButton.jsx
+++ src/components/common/ArrowButton.jsx
@@ -3,10 +3,12 @@ import buttonIcon from "/icon/Icon/-_.svg";
 
 export default function ArrowButton({ text = "See all", onClick, className = "" }) {
   return (
-    <button className={`arrow-button ${className}`} onClick={onClick}>
-      <span className="arrow-button-text text-4">{text}</span>
-      <img src={buttonIcon} alt="arrow" className="arrow-button-icon arrow-button-icon-1" />
-      <img src={buttonIcon} alt="arrow" className="arrow-button-icon arrow-button-icon-2" />
-    </button>
+    <div className={`arrow-button-wrapper ${className}`}>
+      <button className="arrow-button" onClick={onClick}>
+        <span className="arrow-button-text text-4">{text}</span>
+        <img src={buttonIcon} alt="arrow" className="arrow-button-icon arrow-button-icon-1" />
+        <img src={buttonIcon} alt="arrow" className="arrow-button-icon arrow-button-icon-2" />
+      </button>
+    </div>
   );
 }
