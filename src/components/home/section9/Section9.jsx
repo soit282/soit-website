cd61465f3f83ecc6c711ae168618f464b1d75736
dangnam1834 <nam.dang@mirrorfuturediamond.com>
2025-09-22 20:16:16 +0700

    carousel

--- src/components/home/section9/Section9.jsx
+++ src/components/home/section9/Section9.jsx
@@ -28,6 +28,26 @@ export default function Section9() {
       name: "TBros",
       logo: "/1_Homepage/1_Homepage/3_Clients/tra-made.svg",
     },
+    {
+      id: 5,
+      name: "OKKIO",
+      logo: "/1_Homepage/1_Homepage/3_Clients/OKKIO_Logo.svg",
+    },
+    {
+      id: 6,
+      name: "Lêla",
+      logo: "/1_Homepage/1_Homepage/3_Clients/Lêla_logo.svg",
+    },
+    {
+      id: 7,
+      name: "CTY",
+      logo: "/1_Homepage/1_Homepage/3_Clients/CTY_logo.svg",
+    },
+    {
+      id: 8,
+      name: "TBros",
+      logo: "/1_Homepage/1_Homepage/3_Clients/tra-made.svg",
+    },
   ];
 
   // Scroll reveal effect for sticky heading
@@ -90,23 +110,26 @@ export default function Section9() {
             </h2>
           </div>
 
-          <div className="clients-grid">
-            {clients.map((client) => (
-              <div key={client.id} className="client-card">
-                <div
-                  className="client-background"
-                  style={{
-                    backgroundImage: `url('/1_Homepage/1_Homepage/3_Clients/Background_1.png')`,
-                  }}
-                >
-                  <img
-                    src={client.logo}
-                    alt={client.name}
-                    className="client-logo"
-                  />
+          <div className="clients-carousel-wrapper">
+            <div className="clients-carousel-track">
+              {/* Duplicate clients array for seamless loop */}
+              {[...clients, ...clients].map((client, index) => (
+                <div key={`${client.id}-${index}`} className="client-card">
+                  <div
+                    className="client-background"
+                    style={{
+                      backgroundImage: `url('/1_Homepage/1_Homepage/3_Clients/Background_1.png')`,
+                    }}
+                  >
+                    <img
+                      src={client.logo}
+                      alt={client.name}
+                      className="client-logo"
+                    />
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
