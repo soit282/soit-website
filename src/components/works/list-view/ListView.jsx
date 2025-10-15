4cc13213ce35ab407cd8c0b51cc093e154d926cc
dangnam1834 <nam.dang@mirrorfuturediamond.com>
2025-10-15 21:51:19 +0700

    reponsive works page 2

--- src/components/works/list-view/ListView.jsx
+++ src/components/works/list-view/ListView.jsx
@@ -1,29 +1,45 @@
 import { projects } from "@data/projects";
 import "./ListView.css";
+import "@styles/grid-system.css";
 
 export default function ListView() {
   return (
     <div className="list-view">
-      <table className="projects-table">
-        <thead>
-          <tr>
-            <th className="text-8 table-header">Name</th>
-            <th className="text-8 table-header">Category</th>
-            <th className="text-8 table-header">Type</th>
-            <th className="text-8 table-header">Year</th>
-          </tr>
-        </thead>
-        <tbody>
-          {projects.map((project) => (
-            <tr key={project.id} className="project-row">
-              <td className="text-7 project-name">{project.name}</td>
-              <td className="text-8 project-category">{project.category}</td>
-              <td className="text-8 project-type">{project.type}</td>
-              <td className="text-8 project-year">{project.year}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="grid-container">
+        <div className="col-12">
+          <div className="projects-table">
+            {/* Header Row */}
+            <div className="table-header-row">
+              <div className="text-7 table-header table-col-client">Client</div>
+              <div className="text-7 table-header table-col-category">
+                Category
+              </div>
+              <div className="text-7 table-header table-col-type">Type</div>
+              <div className="text-7 table-header table-col-year">Year</div>
+              <div className="row-underline"></div>
+            </div>
+
+            {/* Project Rows */}
+            {projects.map((project) => (
+              <div key={project.id} className="project-row">
+                <div className="text-8 project-name table-col-client">
+                  {project.name}
+                </div>
+                <div className="text-8 project-category table-col-category">
+                  {project.category}
+                </div>
+                <div className="text-8 project-type table-col-type">
+                  {project.type}
+                </div>
+                <div className="text-8 project-year table-col-year">
+                  {project.year}
+                </div>
+                <div className="row-underline"></div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
